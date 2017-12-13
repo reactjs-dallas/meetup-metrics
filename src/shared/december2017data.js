d7e2bd678624fd0c03eb5a87bd438c14167f1f90
Erik Golden <erikgolden7@gmail.com>
2017-12-12 21:04:50 -0600

    double ---> single quotes
    Sorry, you can thank prettier for that!

--- src/shared/december2017data.js
+++ src/shared/december2017data.js
@@ -2,15 +2,15 @@
 //  It should be updated by the people who attend the meetup and make a PR to update the file
 const december2017data = [
   {
-    name: "Mike Mathew",
-    twitter: "drumsensei",
+    name: 'Mike Mathew',
+    twitter: 'drumsensei',
     about:
-      "Father of three, husband of one, writer of JavaScripts, drinker of coffee"
+      'Father of three, husband of one, writer of JavaScripts, drinker of coffee'
   },
   {
-    name: "ReactJS Dallas",
-    twitter: "reactjsdallas",
-    about: "We host events and make sure devs get both knowledge and pizza."
+    name: 'ReactJS Dallas',
+    twitter: 'reactjsdallas',
+    about: 'We host events and make sure devs get both knowledge and pizza.'
   },
   {
     name: 'Jose Flores',
@@ -23,34 +23,34 @@ const december2017data = [
     about: 'DIIIIIIIIIIIIIIIIIIIIIIIIIIIIRK!',
   },
   {
-    name: "Dallas Stars",
-    twitter: "DallasStars",
-    about: "Hat tricks a plenty"
+    name: 'Dallas Stars',
+    twitter: 'DallasStars',
+    about: 'Hat tricks a plenty'
   },
   {
-    name: "Dallas Cowboys",
-    twitter: "dallascowboys",
-    about: "America's team for American-style football"
+    name: 'Dallas Cowboys',
+    twitter: 'dallascowboys',
+    about: 'America's team for American-style football'
   },
   {
-    name: "Texas Rangers",
-    twitter: "Rangers",
-    about: "Bases-ball"
+    name: 'Texas Rangers',
+    twitter: 'Rangers',
+    about: 'Bases-ball'
   },
   {
-    name: "FC Dallas",
-    twitter: "FCDallas",
-    about: "Real football"
+    name: 'FC Dallas',
+    twitter: 'FCDallas',
+    about: 'Real football'
   },
   {
-    name: "Connor Bryan",
+    name: 'Connor Bryan',
     twitter: false,
-    about: "Software & music"
+    about: 'Software & music'
   },
   {
-    name: "Erik Golden",
-    linkedIn: "www.linkedin.com/in/erikgolden7",
-    about: "From Utah, moving to Dallas. Wooo! I love to code!!!"
+    name: 'Erik Golden',
+    linkedIn: 'www.linkedin.com/in/erikgolden7',
+    about: 'From Utah, moving to Dallas. Wooo! I love to code!!!'
   },
     name: 'Joel Lee',
     twitter: 'Highflierinvest',
