9c06ba2969bb9ecbe39d1c38215a51a941461e11
Joel Lee <joel.lee@taillight.com>
2017-12-12 19:38:04 -0600

    Add J Lee attendance

--- src/shared/december2017data.js
+++ src/shared/december2017data.js
@@ -36,6 +36,11 @@ const december2017data = [
     twitter: 'FCDallas',
     about: 'Real football',
   },
+  {
+    name: 'Joel Lee',
+    twitter: 'Highflierinvest',
+    about: 'DFW Area Real Estate Investor'
+  }
 ];
 
 export default december2017data;
