7a7cc41375b93943bcc83a865879f5af1ec5ff30
Bryce Hull <bahull@live.com>
2017-12-12 21:37:18 -0600

    Updated to single quotes

--- src/shared/december2017data.js
+++ src/shared/december2017data.js
@@ -2,50 +2,50 @@
 //  It should be updated by the people who attend the meetup and make a PR to update the file
 const december2017data = [
   {
-    name: "Mike Mathew",
-    twitter: "drumsensei",
+    name: 'Mike Mathew',
+    twitter: 'drumsensei',
     about:
-      "Father of three, husband of one, writer of JavaScripts, drinker of coffee"
+      'Father of three, husband of one, writer of JavaScripts, drinker of coffee'
   },
   {
-    name: "ReactJS Dallas",
-    twitter: "reactjsdallas",
-    about: "We host events and make sure devs get both knowledge and pizza."
+    name: 'ReactJS Dallas',
+    twitter: 'reactjsdallas',
+    about: 'We host events and make sure devs get both knowledge and pizza.'
   },
   {
-    name: "Dallas Mavericks",
-    twitter: "dallasmavs",
-    about: "DIIIIIIIIIIIIIIIIIIIIIIIIIIIIRK!"
+    name: 'Dallas Mavericks',
+    twitter: 'dallasmavs',
+    about: 'DIIIIIIIIIIIIIIIIIIIIIIIIIIIIRK!'
   },
   {
-    name: "Dallas Stars",
-    twitter: "DallasStars",
-    about: "Hat tricks a plenty"
+    name: 'Dallas Stars',
+    twitter: 'DallasStars',
+    about: 'Hat tricks a plenty'
   },
   {
-    name: "Dallas Cowboys",
-    twitter: "dallascowboys",
+    name: 'Dallas Cowboys',
+    twitter: 'dallascowboys',
     about: "America's team for American-style football"
   },
   {
-    name: "Texas Rangers",
-    twitter: "Rangers",
-    about: "Bases-ball"
+    name: 'Texas Rangers',
+    twitter: 'Rangers',
+    about: 'Bases-ball'
   },
   {
-    name: "FC Dallas",
-    twitter: "FCDallas",
-    about: "Real football"
+    name: 'FC Dallas',
+    twitter: 'FCDallas',
+    about: 'Real football'
   },
   {
-    name: "Bryce Hull",
-    twitter: "BryceHull1",
-    about: "New to React Js Dallas"
+    name: 'Bryce Hull',
+    twitter: 'BryceHull1',
+    about: 'New to React Js Dallas'
   },
   {
-    name: "Connor Bryan",
+    name: 'Connor Bryan',
     twitter: false,
-    about: "Software & music"
+    about: 'Software & music'
   }
 ];
 
