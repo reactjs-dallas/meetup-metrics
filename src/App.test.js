439b5915a341a187c9766e8de6653de339768df0
Mike Mathew <m2mathew@me.com>
2017-12-26 00:38:01 -0600

    [Test] Adds enzyme and rewrites test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
+// External Dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from "enzyme";
+
+// Internal Dependencies
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+// Local Variables
+const app = shallow(<App />);
+
+// Begin Tests
+describe('App', () => {
+  it('renders correctly', () => {
+    expect(app).toMatchSnapshot();
+  });
 });
