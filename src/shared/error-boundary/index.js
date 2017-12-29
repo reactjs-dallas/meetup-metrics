dd2d6025d31c48dfb1fff95fa6ed330553151b0d
Mike Mathew <m2mathew@me.com>
2017-12-28 22:00:52 -0600

    [Error] Adds error boundary to the project

---
+++ src/shared/error-boundary/index.js
@@ -0,0 +1,47 @@
+// External Dependencies
+import React, { Component } from 'react';
+
+// Local Variables
+const detailsStyles = {
+  whiteSpace: 'pre-wrap',
+};
+
+// Component Definition
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      errorInfo: null,
+    };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // Catch errors in any components below and re-render with error message
+    this.setState({
+      error: error,
+      errorInfo: errorInfo
+    })
+    // You can also log error messages to an error reporting service here
+  }
+
+  render() {
+    if (this.state.errorInfo) {
+      // Error path
+      return (
+        <div>
+          <h2>Something went wrong.</h2>
+          <details style={detailsStyles}>
+            {this.state.error && this.state.error.toString()}
+            <br />
+            {this.state.errorInfo.componentStack}
+          </details>
+        </div>
+      );
+    }
+    // Normally, just render children
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
