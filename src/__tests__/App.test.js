196763ca1ea0fb6bcd092b39f3319aecd471281a
Peter Miles <hello@petermiles.io>
2018-01-04 12:54:31 -0600

    add linter

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,17 @@
+// External Dependencies
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from "enzyme";
+
+// Internal Dependencies
+import App from '../App';
+
+// Local Variables
+const app = shallow(<App />);
+
+// Begin Tests
+describe('App', () => {
+  it('renders correctly', () => {
+    expect(app).toMatchSnapshot();
+  });
+});
