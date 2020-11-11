bd14a8ae964c6079a08360a5198c62ba21b916f3
Aldi Zhupani <azhupani390@berkeley.edu>
2020-11-10 20:17:21 -0600

    Adding copy right text to footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 
 // Local Dependencies
+import { getCurrentYear } from './utils/date'
 import Attendance from './attendance';
 import ErrorBoundary from './shared/error-boundary';
 import MembershipTrend from './trend';
@@ -30,9 +31,12 @@ class App extends Component {
           </section>
 
           <footer className="app-footer">
-            <img src={logo} className="app-logo" alt="logo" />
-            <p>A community project of <a href="https://www.text-em-all.com/careers" tabIndex={0}>Text-Em-All</a></p>
-            <img src={logo} className="app-logo-2" alt="logo" />
+            <div style={{ display: 'inline-flex' }}>
+              <img src={logo} className="app-logo" alt="logo" />
+              <p>A community project of <a href="https://www.text-em-all.com/careers" tabIndex={0}>Text-Em-All</a></p>
+              <img src={logo} className="app-logo-2" alt="logo" />
+            </div>
+            <p>© {getCurrentYear()} ReactJS Dallas User Group. All Rights Reserved.</p>
           </footer>
         </div>
       </ErrorBoundary>
---
+++ src/utils/date.js
@@ -0,0 +1,7 @@
+export const getCurrentYear = () => {
+  const today = new Date()
+  return today.getFullYear()
+}
+
+// TO DO: Replace with a more useful utility :)
+export const test = () => {}
